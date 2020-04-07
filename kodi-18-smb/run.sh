eb7ecd0d2bc5efdce9552cf8e03b980e56ad3da7
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-07 14:59:55 +0200

    Update run.sh

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -1,4 +1,4 @@
-mkdir -p /share/kodi-gui-new/data >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /share/kodi-gui-new/data /root/.kodi \
+mkdir -p /share/volumes/smb/kodi_18_smb/data >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /share/volumes/smb/kodi_18_smb/data /root/.kodi \
 && mkdir -p /data >/dev/null 2>&1
 
 #export SDL_MOUSE_RELATIVE=0
