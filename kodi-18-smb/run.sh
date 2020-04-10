5474ff5486a66c8f9cbc1c6ef3e915f5dc13a868
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-10 21:15:40 +0200

    make directory structure in not exist - smb

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -1,5 +1,5 @@
 ##
-#make directory structure in not exist
+#make directory structure in not exist - locale
 mkdir -p /share/its
 mkdir -p /share/its/smb_data
 mkdir -p /share/its/cfg
@@ -7,6 +7,12 @@ mkdir -p /share/its/cfg
 #add samba share
 echo '//192.168.0.198/smb$/data /share/its/smb_data cifs rw,credentials=/share/its/cfg/ssh_addon_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770 0 0' >> /etc/fstab
 mount -a
+##
+#make directory structure in not exist - smb
+mkdir -p /share/its/smb_data/data
+##
+#make simbolic link to smb folder
+ln -s /share/its/smb_data/data /root/.kodi
 ##
 #mkdir -p /share/its/smb_data/kodi_18_smb/data >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /share/its/smb_data/kodi_18_smb/data /root/.kodi \
 #&& mkdir -p /data >/dev/null 2>&1
