7ce821bd44c54429b54dfc1309abeddf1453a4ec
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-10 15:54:18 +0200

    added add samba share command

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -1,6 +1,8 @@
-mkdir -p /share/volumes/smb/kodi_18_smb/data >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /share/volumes/smb/kodi_18_smb/data /root/.kodi \
-&& mkdir -p /data >/dev/null 2>&1
+#add samba share
+echo '//192.168.0.198/smb$/data /share/its/smb_data cifs rw,credentials=/share/its/cfg/ssh_addon_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770 0 0' >> /etc/fstab
+mount -a
 
-#export SDL_MOUSE_RELATIVE=0
+mkdir -p /share/its/smb_data/kodi_18_smb/data >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /share/its/smb_data/kodi_18_smb/data /root/.kodi \
+&& mkdir -p /data >/dev/null 2>&1
 
 /usr/bin/kodi
