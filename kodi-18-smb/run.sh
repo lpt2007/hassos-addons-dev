b4ac16deb275b45f07ee0138ff015835ad1de9df
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-11 13:07:36 +0200

    added to mount settings - vers=1.0

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -5,7 +5,7 @@ mkdir -p /share/its/smb_data
 mkdir -p /share/its/cfg
 ##
 #add samba share
-echo '//192.168.0.198/smb$/data /share/its/smb_data cifs rw,credentials=/share/its/cfg/ssh_addon_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770 0 0' >> /etc/fstab
+echo '//192.168.0.198/smb$/data /share/its/smb_data cifs rw,credentials=/share/its/cfg/ssh_addon_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770,vers=1.0 0 0' >> /etc/fstab
 mount -a
 ##
 #make directory structure in not exist - smb
