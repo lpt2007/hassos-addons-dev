69b98b653421d3210fb1ba33eb7b62ef8279c610
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-11 20:26:18 +0200

    Update run.sh

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -9,8 +9,20 @@ mkdir -p /share/its/smb_data
 mkdir -p /share/its/cfg
 ##
 #add samba share
-echo '//192.168.0.198/smb$/data /share/its/smb_data cifs rw,credentials=/ssl/kodi_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770,vers=1.0 0 0' >> /etc/fstab
-mount -a
+smb_ip=$(jq -r '.smb_ip' $OPTIONS_PATH)
+smb_path=$(jq -r '.smb_path' $OPTIONS_PATH)
+if [ "$smb_ip" == "" ]; then
+            echo "[INFO] Samba IP not configured"
+        else
+         if [ "$smb_path" == "" ]; then
+            echo "[INFO] Samba path not configured"
+        else
+            echo "[INFO] Fond Samba Server IP $smb_ip"
+            echo "[INFO] Fond Samba path $smb_path"
+            echo '//$smb_ip/$smb_path /share/its/smb_data cifs rw,credentials=/ssl/kodi_smb.txt,uid=0,gid=0,file_mode=0660,dir_mode=0770,vers=1.0 0 0' >> /etc/fstab
+            mount -a
+         fi
+fi
 ##
 #make directory structure in not exist - smb
 kodi_data=$(jq -r '.kodi_data' $OPTIONS_PATH)
