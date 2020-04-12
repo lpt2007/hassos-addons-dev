6996775c3ce14ba0ea698ff57e53bd4f30d046ab
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-12 22:23:37 +0200

    Update run.sh

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -83,16 +83,16 @@ if [ -d "/share/its/smb_data/$kodi_data/userdata/Database" ]; then
             echo "[INFO] Move files from /share/its/smb_data/$kodi_data/userdata/Database"
             mv /share/its/smb_data/$kodi_data/userdata/Database/* /share/its/loc_data/$kodi_data
             echo "[INFO] Making symbolic link from /root/.kodi to /share/its/smb_data/$kodi_data"
-            ln -s /share/its/smb_data/$kodi_data/userdata/Database /share/its/loc_data/$kodi_data
+            ln -s /share/its/loc_data/$kodi_data /share/its/smb_data/$kodi_data/userdata/Database
         else            
             echo "[INFO] No sqllite database found on samba share."
             echo "[INFO] Create /share/its/smb_data/$kodi_data/userdata/Database folder if not exist"
             mkdir -p /share/its/smb_data/$kodi_data/userdata
-            mkdir -p /share/its/smb_data/$kodi_data/userdata/Database
+#            mkdir -p /share/its/smb_data/$kodi_data/userdata/Database
             echo "[INFO] Create /share/its/loc_data/$kodi_data folder if not exist"
             mkdir -p /share/its/loc_data/$kodi_data
             echo "[INFO] Making symbolic link from /root/.kodi to /share/its/smb_data/$kodi_data"
-            ln -s /share/its/smb_data/$kodi_data/userdata/Database /share/its/loc_data/$kodi_data
+            ln -s  /share/its/loc_data/$kodi_data /share/its/smb_data/$kodi_data/userdata/Database
 fi
 ##
 #run kodi
