74efe7e9106ea4edd2dc918fea04ed7c84733274
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-04-12 23:10:46 +0200

    Update run.sh

--- kodi-18-smb/run.sh
+++ kodi-18-smb/run.sh
@@ -80,10 +80,12 @@ if [ -d "/share/its/smb_data/$kodi_data/userdata/Database" ]; then
             echo "[INFO] Sqlite database found on samba share"
             echo "[INFO] Create /share/its/loc_data/$kodi_data folder if not exist"
             mkdir -p /share/its/loc_data/$kodi_data
-  if [[ -L "/share/its/smb_data/$kodi_data/userdata/Database" && -d "/share/its/smb_data/$kodi_data/userdata/Database" ]]
+if [[ -L "/share/its/smb_data/$kodi_data/userdata/Database" && -d "/share/its/smb_data/$kodi_data/userdata/Database" ]]
 then
     echo "Database is a symlink to a directory skip moveing files"
 else
+    echo "Remove all files from local database directory."
+    rm /share/its/loc_data/$kodi_data/*
     echo "[INFO] Move files from /share/its/smb_data/$kodi_data/userdata/Database"
     mv /share/its/smb_data/$kodi_data/userdata/Database/* /share/its/loc_data/$kodi_data
     echo "[INFO] Remove database folder on samba share"
