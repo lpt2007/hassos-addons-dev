af845f931d3bbb8b214e207dcab3c6d1e001092d
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-10-18 22:14:21 +0200

    Update run.sh

--- tvheadend-new/run.sh
+++ tvheadend-new/run.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+OPTIONS_PATH=/data/options.json
+
+echo "[INFO] setup vlan"
+vlan_id=$(jq -r '.vlan_id' $OPTIONS_PATH)
+vlan_ip=$(jq -r '.vlan_ip' $OPTIONS_PATH)
+vlan_nm=$(jq -r '.vlan_nm' $OPTIONS_PATH)
+route_ip=$(jq -r '.route_ip' $OPTIONS_PATH)
+route_nm=$(jq -r '.route_nm' $OPTIONS_PATH)
+gateway=$(jq -r '.gateway' $OPTIONS_PATH)
+ethernet=$(jq -r '.ethernet' $OPTIONS_PATH)
+
+vconfig add eth0 3999
+ifconfig $ethernet.$vlan_id $vlan_ip netmask $vlan_nm up
+route add -net $route_ip netmask $route_nm dev $ethernet.$vlan_id
+sysctl -w net.ipv4.conf.$ethernet.$vlan_id.rp_filter=0
+sysctl -w net.ipv4.conf.all.rp_filter=0
+(
+usleep 65000000
+route add default gw $gateway $ethernet
+)&
+
 #mkdir -p /share/tvheadend/recordings
 #mkdir -p ~/.wg++
 #ln -sf /share/tvheadend/wg++/guide.xml ~/.wg++/guide.xml
