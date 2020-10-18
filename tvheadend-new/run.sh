6c67e998c3c9839fe7a9f9d195aadb12d970bd9a
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2020-10-18 16:25:48 +0200

    Update run.sh

--- tvheadend-new/run.sh
+++ tvheadend-new/run.sh
@@ -27,5 +27,5 @@
 #echo "0 0 * * * /share/tvheadend/wg++/run.sh" >> /var/spool/cron/root
 #crond
 
-echo "[INFO] Starting TVHeadend"
-/usr/bin/tvheadend --firstrun -u root -g root -c /share/tvheadend
+#echo "[INFO] Starting TVHeadend"
+#/usr/bin/tvheadend --firstrun -u root -g root -c /share/tvheadend
